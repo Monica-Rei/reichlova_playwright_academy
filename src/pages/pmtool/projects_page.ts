285c808ed09072f46430c22e0b93728f5e20fade
monica <monica.reichlova@gmail.com>
2025-09-01 09:41:44 +0200

    add exercises for atomic tests

--- src/pages/pmtool/projects_page.ts
+++ src/pages/pmtool/projects_page.ts
@@ -1,17 +1,21 @@
-import { Locator, Page } from "@playwright/test";
+import { expect, Locator, Page } from "@playwright/test";
 import { CreateNewProjectModal } from "./projects/create_new_project_modal";
 
 export class ProjectsPage {
   readonly page: Page;
   readonly addProjectButton: Locator;
+  private readonly projectListDiv: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.addProjectButton = page.locator('//button[@test_id="Add Project"]');
+    this.projectListDiv = page.locator("#slimScroll");
   }
 
   async clickAddProject(): Promise<CreateNewProjectModal> {
-    await this.page.waitForTimeout(1000);
+    // ? Čekáme na zobrazení tabulky s projekty, abychom se ujistili, že je stránka načtena
+    //await this.page.waitForTimeout(1000);
+    await expect(this.projectListDiv).toBeVisible();
     await this.addProjectButton.click();
     return new CreateNewProjectModal(this.page);
   }
